3a9b2bfe9ec13fd5edf25f12f774f054c4f09cc8
thebrandstudio <allan@bstt.mx>
2022-12-01 11:51:43 -0600

    Update wedoc_function.php

--- inc/wedoc_function.php
+++ inc/wedoc_function.php
@@ -37,14 +37,7 @@ function single_single_page_sidebar() {
         <h3 class="widget-title"><?php echo get_post_field( 'post_title', $parent, 'display' ); ?></h3>
 
         <?php if ( $children ) { ?>
-          <script>
-          	$(document).on('click', '.doc-sidebar-menu .doc-nav-list li a', function (e) {
-          		e.preventDefault();
-              setTimeout(() => {
-                document.location.reload();
-              }, 900);
-          	});
-          </script>
+
 
             <ul id="prueba" class="doc-nav-list dddd">
                 <?php echo wp_kses_post($children); ?>
