053f8df272ab442d535409e885b377ec4198ce89
thebrandstudio <allan@bstt.mx>
2022-12-01 10:47:54 -0600

    Update wedoc_function.php

--- inc/wedoc_function.php
+++ inc/wedoc_function.php
@@ -37,7 +37,7 @@ function single_single_page_sidebar() {
         <h3 class="widget-title"><?php echo get_post_field( 'post_title', $parent, 'display' ); ?></h3>
 
         <?php if ( $children ) { ?>
-          <a href='index.php?hello=false'>Run PHP Function</a>
+          <a href='index.php?hello=true'>Run PHP Function</a>
             <ul id="prueba" class="doc-nav-list dddd">
                 <?php echo wp_kses_post($children); ?>
             </ul>
