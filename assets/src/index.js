7d233c36d318521449f22bf17672fceaeb078588
thebrandstudio <allan@bstt.mx>
2022-11-30 18:01:19 -0600

    Update index.js

--- assets/src/index.js
+++ assets/src/index.js
@@ -157,8 +157,7 @@ jQuery(function ($) {
 
 		jQuery.post(ddoc_single_ajax_call.ajaxurl, data, function (response) {
 			let obj = JSON.parse(response);
-			display_title.html(obj.post_title);
-			dispaly_container.html(obj.post_content);
+
 			window.history.pushState(null, '', current_url);
 			window.alert('Okay, if youre sure.');
 			progressbar.css('width', '100%');
