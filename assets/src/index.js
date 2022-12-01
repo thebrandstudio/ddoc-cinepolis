463a14e2f0a3113cc379eb5e9f8e4b9d74c547ba
thebrandstudio <allan@bstt.mx>
2022-12-01 17:59:44 -0600

    Update index.js

--- assets/src/index.js
+++ assets/src/index.js
@@ -126,6 +126,8 @@ jQuery(function ($) {
 			var has = $this.parents('li').hasClass('wd-state-open');
 			$('.doc-nav-list > li').removeClass('wd-state-open');
 			if (has) {
+				$this.parents('li').removeClass('wd-state-open');
+			} else {
 				$this.parents('li').addClass('wd-state-open');
 			}
 		});
