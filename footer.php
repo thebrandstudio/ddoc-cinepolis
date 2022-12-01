145eba86fa5b5c46472e1ba6df35a877668aab6e
thebrandstudio <allan@bstt.mx>
2022-12-01 14:50:39 -0600

    Update footer.php

--- footer.php
+++ footer.php
@@ -22,13 +22,13 @@ do_action('_ddoc_footer_content', $footer);
 
 <?php wp_footer(); ?>
 
-  <script>
+<?php
     window.addEventListener('DOMContentLoaded', function () {
       doStuff(function () {
         document.body.className = 'visible';
       });
     }, false);
-  </script>
+?>
   
 </body>
 </html>
