31c6d2afafe218c236a50a00ebdd3f6f742ff28f
thebrandstudio <allan@bstt.mx>
2022-12-01 13:31:29 -0600

    Update wedoc_function.php

--- inc/wedoc_function.php
+++ inc/wedoc_function.php
@@ -38,7 +38,7 @@ function single_single_page_sidebar() {
             		e.preventDefault();
                 setTimeout(() => {
                   document.location.reload();
-                }, 480);
+                }, 490);
             	});
             </script>
             <ul class="doc-nav-list dddd">
