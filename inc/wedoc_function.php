f610c5346596cca72a9e9452fd4ba670cf7e4bd7
thebrandstudio <allan@bstt.mx>
2022-12-19 10:49:57 -0600

    Update wedoc_function.php

--- inc/wedoc_function.php
+++ inc/wedoc_function.php
@@ -43,11 +43,6 @@ function single_single_page_sidebar() {
               		$('.doc-nav-list > li.wd-state-open').find('.children').slideDown(700);
               	}
 
-                if ($('.doc-nav-list > li ul li').hasClass('page_item_has_children')) {
-                  $('.doc-nav-list > li ul li.page_item_has_children').addClass('wd-state-closed');
-                  $('.doc-nav-list > li ul li.page_item_has_children').find('.children').slideUp();
-              	}
-
                 if ('.doc-sidebar-menu') {
                   $(document).on('click', '.doc-sidebar-menu .doc-nav-list li a', function (e) {
                 		e.preventDefault();
